e053fb34293d29a09eccba19e9a1bff1b975792e
VitorVts <vitorgguimaraes56@gmail.com>
2023-04-15 18:23:16 -0300

    Feito alterações no bottão do dark mode

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -4,16 +4,16 @@ const modo = document.getElementById('mode__icon');
     const form = document.getElementById("body__form");
     const tittle = document.getElementById("titulo__principalh1")
     
-    if(modo.classList.contains('fa-toggle-on')){
-       modo.classList.remove('fa-toggle-on');
-       modo.classList.add('fa-toggle-off') ;
+    if(modo.classList.contains('fa-toggle-off')){
+       modo.classList.remove('fa-toggle-off');
+       modo.classList.add('fa-toggle-on') ;
 
        form.classList.add('dark');
        tittle.classList.add('dark');
        return;
     }
-    modo.classList.add('fa-toggle-on');
-    modo.classList.remove('fa-toggle-off');
+    modo.classList.add('fa-toggle-off');
+    modo.classList.remove('fa-toggle-on');
     form.classList.remove("dark");
     tittle.classList.remove('dark');
 });
